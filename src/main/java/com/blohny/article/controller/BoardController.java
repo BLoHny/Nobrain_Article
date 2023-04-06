1baacbe99ef58a10a88ed8e66b95fe9c4798c0b4
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-06 19:43:47 +0900

    경로 설정

--- src/main/java/com/blohny/article/controller/BoardController.java
+++ src/main/java/com/blohny/article/controller/BoardController.java
@@ -1,4 +1,14 @@
 package com.blohny.article.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BoardController {
+
+    @GetMapping("/board/write")
+    public String BoardWriteForm() {
+
+        return "boardwrite";
+    }
 }
