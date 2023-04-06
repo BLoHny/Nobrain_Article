5366d421c015e6e03d72a898462f1bd93a7fcec6
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-06 20:57:03 +0900

    수정기능

--- src/main/java/com/blohny/article/controller/BoardController.java
+++ src/main/java/com/blohny/article/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.blohny.article.controller;
 
 import com.blohny.article.domain.Board;
+import com.blohny.article.repository.BoardRepository;
 import com.blohny.article.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,8 @@ public class BoardController {
 
     @Autowired
     private BoardService boardService;
+    @Autowired
+    private BoardRepository boardRepository;
 
     @GetMapping("/board/write")
     public String BoardWriteForm() {
@@ -26,7 +29,7 @@ public class BoardController {
     public String boardWritePro(Board board) {
 
         boardService.write(board);
-        return "";
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/list")
@@ -50,5 +53,29 @@ public class BoardController {
         boardService.deleteBoard(id);
 
         return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}") //@Integer 형태로 들어온다
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/modify/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(boardTemp.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "에러다 에러 에러!!";
     }
 }
